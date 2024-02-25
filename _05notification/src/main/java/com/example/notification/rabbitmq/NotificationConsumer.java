6fef68a6ba9c1cf9cf3a6ce47a2f893b0e1a29d9
fateme <fatemeh.s2003@gmail.com>
2024-02-25 10:39:29 +0330

    docker compose with mysql container and volume

--- _05notification/src/main/java/com/example/notification/rabbitmq/NotificationConsumer.java
+++ _05notification/src/main/java/com/example/notification/rabbitmq/NotificationConsumer.java
@@ -1,2 +1,19 @@
-package com.example.notification.rabbitmq;public class NotificationConsumer {
+package com.example.notification.rabbitmq;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+//@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    /*@Autowired
+    private NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue}")
+    public void consume(NotificationRequest notificationRequest) {
+        log.warn("=====> consumed {} from queue ", notificationRequest);
+        notificationService.send(notificationRequest);
+    }*/
 }
--- _07amqp/src/main/java/org/example/RabbitMQMessageProducer.java
+++ _07amqp/src/main/java/org/example/RabbitMQMessageProducer.java
@@ -1,2 +1,18 @@
-package org.example;public class RabbitMQMessageProducer {
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class RabbitMQMessageProducer {
+    private final AmqpTemplate amqpTemplate;
+
+    public void publish(Object data, String exchange, String routingKey) {
+        amqpTemplate.convertAndSend(exchange, routingKey, data);
+        log.warn("=====> published to {} by routing key {} with data {}",exchange,routingKey, data);
+    }
 }
